4c4e6fd10ad22a4a7f5a758a34458bd81b354d5e
Emnet19 <emnbef456@gmail.com>
2026-04-27 01:03:57 +0300

    setup

---
+++ app/recipe/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { notFound } from 'next/navigation'
+import { recipes } from '@/lib/recipes'
+import { RecipeDetail } from '../../../components/recipe-detail'
+
+interface RecipePageProps {
+  params: Promise<{ id: string }>
+}
+
+export default async function RecipePage({ params }: RecipePageProps) {
+  const { id } = await params
+  const recipe = recipes.find((r) => r.id === id)
+
+  if (!recipe) {
+    notFound()
+  }
+
+  return <RecipeDetail recipe={recipe} />
+}
